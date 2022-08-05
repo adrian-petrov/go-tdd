82c928956600044aa0840b7da5b0d8a3e215d8f9
Adrian Petrov <adrian@adrianpetrov.com>
2022-08-05 13:56:17 +0100

    mocking

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"gotdd/dependencyinjection"
-	"log"
-	"net/http"
+	"gotdd/mocking"
+	"os"
+	"time"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(dependencyinjection.MyGreeterHandler)))
+	sleeper := &mocking.ConfigurableSleeper{}
+	sleeper.SetDuration(1 * time.Second)
+	sleeper.SetSleep(time.Sleep)
+	mocking.Countdown(os.Stdout, sleeper)
+	// log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(dependencyinjection.MyGreeterHandler)))
 }
---
+++ mocking/countdown.go
@@ -0,0 +1,64 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (cs *ConfigurableSleeper) SetDuration(duration time.Duration) {
+	cs.duration = duration
+}
+
+func (cs *ConfigurableSleeper) SetSleep(sleep func(time.Duration)) {
+	cs.sleep = sleep
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
