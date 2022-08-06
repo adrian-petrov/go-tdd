a92f5895ba784d9b9f859735e9c9b9107f0f0d51
Adrian Petrov <adrian@adrianpetrov.com>
2022-08-06 22:02:14 +0100

    select

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gotdd/httpselect"
 	"gotdd/mocking"
 	"os"
 	"time"
@@ -11,5 +12,6 @@ func main() {
 	sleeper.SetDuration(1 * time.Second)
 	sleeper.SetSleep(time.Sleep)
 	mocking.Countdown(os.Stdout, sleeper)
+	httpselect.Racer("https://facebook.com", "https://github.com")
 	// log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(dependencyinjection.MyGreeterHandler)))
 }
