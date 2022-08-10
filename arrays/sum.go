8c21768a22a462aec1efa282532eb2346d7bed41
Adrian Petrov <adrian@adrianpetrov.com>
2022-08-10 14:36:21 +0100

    generics

--- arrays/sum.go
+++ arrays/sum.go
@@ -3,10 +3,6 @@ package arrays
 func Sum(numbers []int) int {
 	var result int
 
-	// basic for loop
-	// for i := 0; i < len(numbers); i++ {
-	// 	result += numbers[i]
-	// }
 	for _, number := range numbers {
 		result += number
 	}
